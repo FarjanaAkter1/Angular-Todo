70c9fa33800d40dd6e4eae9c1aa5b8dc853e8955
Farjana Akter <fakter@xpanxion.com>
2023-09-30 21:33:43 -0400

    update my angular app

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -8,6 +8,7 @@ import {Todo} from './../../models/Todo';
 export class TodosComponent implements OnInit {
 
   todos!:Todo[]
+  inputTodo:string="";
   constructor() { }
 
   ngOnInit():void{
@@ -37,5 +38,13 @@ completed: false
   deleteTodo (id:number) {
 
     this.todos= this.todos.filter((v,i)=> i  !== id);
+  }
+  addTodo (){
+    this.todos.push({
+      content: this.inputTodo,
+      completed:false
+      
+    })
+    this.inputTodo=""
   }
 }
